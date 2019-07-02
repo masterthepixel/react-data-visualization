c114307fc3795b6c15c40e990152553bc4b711b7
masterthepixel <masterthepixel@gmail.com>
2019-07-02 08:23:10 -0300

    Responsible coins grid

--- src/components/CoinGrid/styles.js
+++ src/components/CoinGrid/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(130px, 1fr));
   grid-gap: 15px;
 `;
 
